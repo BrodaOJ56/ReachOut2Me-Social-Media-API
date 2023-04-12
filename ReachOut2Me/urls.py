ed3834eb37b7adcc0beeaea5aa0e8cc1e6347e0c
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-04-12 23:22:37 +0100

    added user_profile endpoint

--- ReachOut2Me/urls.py
+++ ReachOut2Me/urls.py
@@ -1,7 +1,8 @@
-from .endpoints.users import GetAllUsers, RegisterUser
+from .endpoints.users import GetAllUsers, RegisterUser, UserProfileView
 from django.urls import path
 
 urlpatterns = [
     path('users', GetAllUsers.as_view(), name='get_all_users'),
     path('register', RegisterUser.as_view(), name='register_user'),
+    path('profile', UserProfileView.as_view(), name='user_profile'),
 ]
