657f346296fbbd3899e6afc3425b9e38db97873d
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-13 03:24:50 +0100

    the add avatar class

--- ReachOut2Me/endpoints/users.py
+++ ReachOut2Me/endpoints/users.py
@@ -2,7 +2,7 @@ from ..models import User, UserProfile
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from ..serializers import UserProfileSerializer
+from ..serializers import UserProfileSerializer, UploadAvatarSerializer
 from ..utils import validate_email
 
 
@@ -16,16 +16,22 @@ class GetAllUsers(APIView):
                 user.email = user.email
             else:
                 user.email = 'No email provided'
+            if hasattr(user, 'userprofile') and user.userprofile.avatar:
+                avatar = user.userprofile.avatar.url
+            else:
+                avatar = 'No avatar available'
+            if hasattr(user, 'userprofile') and user.userprofile.bio:
+                bio = user.userprofile.bio
+            else:
+                bio = 'No bio available'
             all_users.append({
+                'id': user.id,
                 'username': user.username,
                 'email': user.email,
-                'is_staff': user.is_staff,
-                'is_active': user.is_active,
                 'is_superuser': user.is_superuser,
                 'date_joined': user.date_joined,
-                'last_login': user.last_login,
-                'avatar': user.userprofile.avatar.url if hasattr(user, 'userprofile') else 'No avatar available',
-                'bio': user.userprofile.bio if hasattr(user, 'userprofile') else 'No bio available',
+                'avatar': avatar,
+                'bio': bio
             })
         return Response(all_users, status=status.HTTP_200_OK)
 
@@ -52,6 +58,7 @@ class RegisterUser(APIView):
         data['first_name'] = data['first_name'].lower()
         data['last_name'] = data['last_name'].lower()
         user = User.objects.create_user(**data)
+        UserProfile.objects.create(user=user)
         return Response({'success': f'User {user.username} registered.'}, status=status.HTTP_201_CREATED)
 
 
@@ -73,5 +80,26 @@ class UserProfileView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# we will implement the current user when we implement authentication/login
+class UploadAvatarView(APIView):
+    def put(self, request, pk):
+        # user_profile = UserProfile.objects.filter(user=request.user).first()
+        user_profile = User.objects.get(id=pk)
+        if not user_profile:
+            return Response({'error': 'User profile not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        user_avatar = UserProfile.objects.filter(user=user_profile).first()
+        # if not user_avatar:
+        #     UserProfile.objects.create(user=user_profile)
+        #     user_avatar = UserProfile.objects.filter(user=user_profile).first()
+
+        serializer = UploadAvatarSerializer(user_avatar, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'success': 'Avatar uploaded successfully.'}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
