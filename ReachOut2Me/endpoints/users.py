c2d585a17a7eec847d8a016d37b7965b6f49e607
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-14 00:59:29 +0100

    used serializer on the get all users method

--- ReachOut2Me/endpoints/users.py
+++ ReachOut2Me/endpoints/users.py
@@ -2,38 +2,41 @@ from ..models import User, UserProfile
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from ..serializers import UserProfileSerializer, UploadAvatarSerializer
+from ..serializers import UserProfileSerializer, UploadAvatarSerializer, UserProfile_Serializer
 from ..utils import validate_email
 
 
 # Just testing out the APIView for the get all users endpoint
 class GetAllUsers(APIView):
     def get(self, request):
-        users = User.objects.select_related('userprofile').all()
-        all_users = []
-        for user in users:
-            if user.email:
-                user.email = user.email
-            else:
-                user.email = 'No email provided'
-            if hasattr(user, 'userprofile') and user.userprofile.avatar:
-                avatar = user.userprofile.avatar.url
-            else:
-                avatar = 'No avatar available'
-            if hasattr(user, 'userprofile') and user.userprofile.bio:
-                bio = user.userprofile.bio
-            else:
-                bio = 'No bio available'
-            all_users.append({
-                'id': user.id,
-                'username': user.username,
-                'email': user.email,
-                'is_superuser': user.is_superuser,
-                'date_joined': user.date_joined,
-                'avatar': avatar,
-                'bio': bio
-            })
-        return Response(all_users, status=status.HTTP_200_OK)
+        # users = User.objects.select_related('userprofile').all()
+        users = UserProfile.objects.all()
+        serializer = UserProfile_Serializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        # all_users = []
+        # for user in users:
+        #     if user.email:
+        #         user.email = user.email
+        #     else:
+        #         user.email = 'No email provided'
+        #     if hasattr(user, 'userprofile') and user.userprofile.avatar:
+        #         avatar = user.userprofile.avatar.url
+        #     else:
+        #         avatar = 'No avatar available'
+        #     if hasattr(user, 'userprofile') and user.userprofile.bio:
+        #         bio = user.userprofile.bio
+        #     else:
+        #         bio = 'No bio available'
+        #     all_users.append({
+        #         'id': user.id,
+        #         'username': user.username,
+        #         'email': user.email,
+        #         'is_superuser': user.is_superuser,
+        #         'date_joined': user.date_joined,
+        #         'avatar': avatar,
+        #         'bio': bio
+        #     })
+        # return Response(all_users, status=status.HTTP_200_OK)
 
 
 # Endpoint to register as a new user
@@ -88,7 +91,8 @@ class UserProfileView(APIView):
 class UploadAvatarView(APIView):
     def put(self, request, pk):
         # user_profile = UserProfile.objects.filter(user=request.user).first()
-        user_profile = User.objects.get(id=pk)
+        # user_profile = User.objects.get(id=pk)
+        user_profile = User.objects.filter(id=pk).first()   # This works fine to display the error message
         if not user_profile:
             return Response({'error': 'User profile not found.'}, status=status.HTTP_404_NOT_FOUND)
 
