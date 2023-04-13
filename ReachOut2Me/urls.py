33cfc312882ac4ac03be27e6c37f2058cab4701f
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-13 03:24:19 +0100

    registeretd the url

--- ReachOut2Me/urls.py
+++ ReachOut2Me/urls.py
@@ -1,8 +1,11 @@
-from .endpoints.users import GetAllUsers, RegisterUser, UserProfileView
+from .endpoints.users import *
 from django.urls import path
 
 urlpatterns = [
     path('users/', GetAllUsers.as_view(), name='get_all_users'),
     path('register/', RegisterUser.as_view(), name='register_user'),
     path('profile/', UserProfileView.as_view(), name='user_profile'),
+    # path('avatar/<str:first_name>/<str:last_name>/', UploadAvatarView.as_view(), name='user_avatar'),
+    path('avatar/<int:pk>/', UploadAvatarView.as_view(), name='user_avatar'),
+    # path('avatar/<string:first_name>/<string:last_name>/', UploadAvatarView.as_view(), name='user_avatar'),
 ]
