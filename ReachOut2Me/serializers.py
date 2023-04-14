8c79ad75f3f2fdac15d65676e005006d217c3862
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-14 01:01:05 +0100

    serializer to display the user profile details

--- ReachOut2Me/serializers.py
+++ ReachOut2Me/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers, viewsets
-from .models import Post, Comment, Message, FriendRequest,  UserProfile
+from .models import Post, Comment, Message, FriendRequest,  UserProfile, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -45,3 +45,25 @@ class UploadAvatarSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ('avatar',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
+
+class UserProfile_Serializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username')
+    email = serializers.EmailField(source='user.email')
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+    date_joined = serializers.DateTimeField(source='user.date_joined')
+
+    class Meta:
+        model = UserProfile
+        fields = ['id', 'username', 'email',
+                  'first_name', 'last_name', 'date_of_birth',
+                  'avatar', 'bio', 'date_joined', 'gender',
+                  'country', 'state_or_city', 'telephone_number'
+                  ]
