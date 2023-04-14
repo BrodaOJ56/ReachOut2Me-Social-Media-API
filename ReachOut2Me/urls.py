d60d530ec464ea35ac09ca1859084e4432e55b56
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-04-15 00:50:28 +0100

    added posts url endpoint

--- ReachOut2Me/urls.py
+++ ReachOut2Me/urls.py
@@ -1,6 +1,6 @@
 from .endpoints.users import *
 from django.urls import path
-from .endpoints.posts import PostListCreateView
+from .endpoints.posts import PostListCreateView, PostDetailView
 
 urlpatterns = [
     path('users/', GetAllUsers.as_view(), name='get_all_users'),
@@ -9,5 +9,8 @@ urlpatterns = [
     # path('avatar/<str:first_name>/<str:last_name>/', UploadAvatarView.as_view(), name='user_avatar'),
     path('avatar/<int:pk>/', UploadAvatarView.as_view(), name='user_avatar'),
     # path('avatar/<string:first_name>/<string:last_name>/', UploadAvatarView.as_view(), name='user_avatar'),
+    #retrieve a list of posts or create a new post by sending a POST request with the required data in the request body
     path('posts/', PostListCreateView.as_view(), name='post_list_create'),
+    #allows users to retrieve a single post using the GET method
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]
