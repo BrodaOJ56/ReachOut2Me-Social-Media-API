bc0afbb01f5ebc13b1be0dbd331b792a58c2fda1
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-04-15 00:49:29 +0100

    allows users to retrieve a single post using the GET method

--- ReachOut2Me/endpoints/posts.py
+++ ReachOut2Me/endpoints/posts.py
@@ -8,3 +8,7 @@ class PostListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
