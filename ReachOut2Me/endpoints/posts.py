1441d822f4fce5586659f3f6b55bc9c55a6e0560
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-04-15 00:40:03 +0100

    user can get posts and create posts

---
+++ ReachOut2Me/endpoints/posts.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from ..models import Post
+from ..serializers import PostSerializer
+
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
