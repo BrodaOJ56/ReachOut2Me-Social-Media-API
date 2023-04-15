c3ad95a1a17df82cf7a196cc349a9b6b72f765b9
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-04-15 01:27:38 +0100

    added posts like endpoint

--- ReachOut2Me/urls.py
+++ ReachOut2Me/urls.py
@@ -1,6 +1,6 @@
 from .endpoints.users import *
 from django.urls import path
-from .endpoints.posts import PostListCreateView, PostDetailView
+from .endpoints.posts import PostListCreateView, PostDetailView, PostLikeView
 
 urlpatterns = [
     path('users/', GetAllUsers.as_view(), name='get_all_users'),
@@ -11,6 +11,7 @@ urlpatterns = [
     # path('avatar/<string:first_name>/<string:last_name>/', UploadAvatarView.as_view(), name='user_avatar'),
     #retrieve a list of posts or create a new post by sending a POST request with the required data in the request body
     path('posts/', PostListCreateView.as_view(), name='post_list_create'),
-    #allows users to retrieve a single post using the GET method
+    #allows users to retrieve a single post using the GET method and update using PUT method
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:pk>/like/', PostLikeView.as_view(), name='post_like'),
 ]
