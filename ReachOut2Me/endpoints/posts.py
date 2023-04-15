abc906715eb1008a455948763646283e52b8f6d7
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-04-15 22:52:10 +0100

    create comment on a post functionality

--- ReachOut2Me/endpoints/posts.py
+++ ReachOut2Me/endpoints/posts.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, status
-from ..models import Post
+from ..models import Post, Comment
 from rest_framework.response import Response
-from ..serializers import PostSerializer
+from ..serializers import PostSerializer, CommentSerializer
+from rest_framework.views import APIView
 
 class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -34,3 +35,18 @@ class PostLikeView(generics.GenericAPIView):
         post.save()
         serializer = self.get_serializer(post)
         return Response({'post': serializer.data, 'message': message}, status=status.HTTP_200_OK)
+    
+class CreateComment(APIView):
+    serializer_class = CommentSerializer
+
+    def post(self, request, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return Response({'error': 'Post not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save(post=post)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
