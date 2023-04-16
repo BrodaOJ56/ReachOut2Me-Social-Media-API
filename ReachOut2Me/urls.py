3d1d455c93ecb6383931b2addd0cd401f4e9ecc7
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-16 20:32:03 +0100

    modify the urls of the avatar upload to work with request.user

--- ReachOut2Me/urls.py
+++ ReachOut2Me/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register_user'),
     path('profile/', UserProfileView.as_view(), name='user_profile'),
     # path('avatar/<str:first_name>/<str:last_name>/', UploadAvatarView.as_view(), name='user_avatar'),
-    path('avatar/<int:pk>/', UploadAvatarView.as_view(), name='user_avatar'),
+    path('avatar/', UploadAvatarView.as_view(), name='user_avatar'),
     # path('avatar/<string:first_name>/<string:last_name>/', UploadAvatarView.as_view(), name='user_avatar'),
     # retrieve a list of posts or create a new post by sending a POST request with the required data in the request body
     path('posts/', PostListCreateView.as_view(), name='post_list_create'),
