50a7c58520d4c63702f77ab97ef5a01cea244fd6
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-23 13:27:37 +0100

    add the test directory to the installed apps

--- SocialMedia_project/settings.py
+++ SocialMedia_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'ReachOut2Me.tests',
 
     'ReachOut2Me.apps.Reachout2MeConfig', # new
     #'ReachOut2Me'
