520c6de84d85f75719d2f7d6becf2d8c36b226a2
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-24 15:10:59 +0100

    test search by username

--- ReachOut2Me/tests/test_profile.py
+++ ReachOut2Me/tests/test_profile.py
@@ -28,3 +28,16 @@ class ProfileTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['bio'], None)
         self.assertEqual(response.data['user'], self.user.id)
+
+    def test_search_by_username(self):
+        """Test the api can get user profile by username."""
+        response = self.client.get(reverse("search_user", kwargs={"username": "testuser"}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['first_name'], 'testfirstname')
+
+    # def test_update_user_profile(self):
+    #     """Test the api can update user profile."""
+    #     response = self.client.put(self.url, data={"bio": "test bio"})
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #     self.assertEqual(response.data['bio'], 'test bio')
+    #     self.assertEqual(response.data['user'], self.user.id)
