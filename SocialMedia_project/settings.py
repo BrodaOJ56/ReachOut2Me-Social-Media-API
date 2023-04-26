b14cea7b0caec8e76f3adba89020703e3bb1be90
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-04-26 06:40:07 +0100

    hosting commit

--- SocialMedia_project/settings.py
+++ SocialMedia_project/settings.py
@@ -147,6 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
