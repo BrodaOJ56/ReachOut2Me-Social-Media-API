c2ba9b1a752341e2e562aee4f73a493a7341b0a2
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-27 17:37:15 +0100

    reformatted

--- ReachOut2Me/tests/test_profile.py
+++ ReachOut2Me/tests/test_profile.py
@@ -11,42 +11,51 @@ class ProfileTestCase(TestCase):
     """This class defines the test suite for the post model."""
 
     def setUp(self):
-        self.url = reverse('user_profile')
+        self.url = reverse("user_profile")
         """Define the test client and other test variables."""
-        self.user = User.objects.create_user(username='testuser', password='testpassword',
-                                             first_name='testfirstname', last_name='testlastname',
-                                             email='test@email.com'
-                                             )
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="testpassword",
+            first_name="testfirstname",
+            last_name="testlastname",
+            email="test@email.com",
+        )
         UserProfile.objects.create(user=self.user)
         self.token = Token.objects.create(user=self.user)
         self.client = APIClient()
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
 
     def test_get_user_profile(self):
         """Test the api can get user profile."""
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['bio'], None)
-        self.assertEqual(response.data['user'], self.user.id)
+        self.assertEqual(response.data["bio"], None)
+        self.assertEqual(response.data["user"], self.user.id)
 
     def test_search_by_username(self):
         """Test the api can get user profile by username."""
-        response = self.client.get(reverse("search_user", kwargs={"username": "testuser"}))
+        response = self.client.get(
+            reverse("search_user", kwargs={"username": "testuser"})
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['first_name'], 'testfirstname')
+        self.assertEqual(response.data["first_name"], "testfirstname")
 
     def test_update_user_profile(self):
         """Test the api can update user profile."""
-        response = self.client.put(self.url, data={"bio": "test bio",
-                                                   "country": "france",
-                                                   })
+        response = self.client.put(
+            self.url,
+            data={
+                "bio": "test bio",
+                "country": "france",
+            },
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['bio'], 'test bio')
-        self.assertEqual(response.data['country'], 'france')
-        self.assertEqual(response.data['user'], self.user.id)
+        self.assertEqual(response.data["bio"], "test bio")
+        self.assertEqual(response.data["country"], "france")
+        self.assertEqual(response.data["user"], self.user.id)
 
     def test_get_user_account(self):
         """Test the api can get user account."""
         response = self.client.get(reverse("get_user_profile"))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['username'], 'testuser')
+        self.assertEqual(response.data["username"], "testuser")
