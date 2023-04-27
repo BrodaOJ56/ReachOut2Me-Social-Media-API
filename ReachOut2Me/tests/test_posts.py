5bd1ec878b286b9f90ddb9224c929d5ab508b4c2
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-27 15:13:24 +0100

    test for test_get_a_post_by_id

--- ReachOut2Me/tests/test_posts.py
+++ ReachOut2Me/tests/test_posts.py
@@ -3,7 +3,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
 from django.contrib.auth.models import User
-from ..models import UserProfile
+from ..models import UserProfile, Post
 from rest_framework.authtoken.models import Token
 
 
@@ -19,6 +19,7 @@ class PostTestCase(TestCase):
                                                 )
         UserProfile.objects.create(user=self.user)
         self.token = Token.objects.create(user=self.user)
+        Post.objects.create(author=self.user, content="test content")
         self.client = APIClient()
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
@@ -26,13 +27,20 @@ class PostTestCase(TestCase):
         """Test the api can get all posts."""
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 0)
-        self.assertEqual(response.data, [])
+        self.assertEqual(len(response.data), 1)
 
     def test_create_post(self):
         """Test the api can create a post."""
         response = self.client.post(self.url, data={"content": "test content"
                                                     })
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['content'], 'test content')
+        self.assertEqual(response.data['author'], self.user.username)
+
+    def test_get_a_post_by_id(self):
+        """Test the api can get a post by id."""
+        url = reverse("post_detail", kwargs={"pk": 1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['content'], 'test content')
         self.assertEqual(response.data['author'], self.user.username)
