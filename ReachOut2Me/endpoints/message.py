f8902f4aa93f83dee2ee30135f8453ad043e57d8
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-04-30 01:17:54 +0100

    message feature

--- ReachOut2Me/endpoints/message.py
+++ ReachOut2Me/endpoints/message.py
@@ -1,1 +1,17 @@
+from rest_framework import generics
 from ..models import Message
+from ..serializers import MessageSerializer
+
+class MessageList(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+class MessageDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
