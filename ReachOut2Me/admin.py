eed6a337dac1d96ce1bb6fe384a535c21fbfa0be
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-05-01 23:56:05 +0100

    registered follow table

--- ReachOut2Me/admin.py
+++ ReachOut2Me/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from .models import Post, Comment, Message, FriendRequest, UserProfile,CommentReply,Notification
+from .models import Post, Comment, Message, UserProfile,CommentReply,Notification, Follow
 
 # registered models with the admin site
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Message)
-admin.site.register(FriendRequest)
 admin.site.register(UserProfile)
 admin.site.register(CommentReply)
 admin.site.register(Notification)
+admin.site.register(Follow)
