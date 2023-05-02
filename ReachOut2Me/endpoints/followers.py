84ecf6eb0f7e933a9386412f827bbaba1cd77e33
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-05-02 22:18:17 +0100

    comments on unfollow_user

--- ReachOut2Me/endpoints/followers.py
+++ ReachOut2Me/endpoints/followers.py
@@ -40,26 +40,39 @@ def follow_user(request, user_id):
     return Response({"success": "User followed successfully"}, status=status.HTTP_200_OK)
 
 
-
+# Decorator indicates that this function can handle HTTP POST requests
 @api_view(['POST'])
+# Takes the request object and the ID of the user to unfollow as parameters
 def unfollow_user(request, user_id):
     try:
+        # Try to retrieve the user to unfollow by ID
         user_to_unfollow = User.objects.get(id=user_id)
     except User.DoesNotExist:
+        # If the user is not found, return a 404 error response
         return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-    
+
+    # Retrieve the current user from the request object
     current_user = request.user
+
+    # Check if the current user is the same as the user to unfollow
     if current_user == user_to_unfollow:
+        # If so, return a 400 error response
         return Response({"error": "You can't unfollow yourself"}, status=status.HTTP_400_BAD_REQUEST)
-    
+
+    # Retrieve the user profile for the current user
     current_user_profile = UserProfile.objects.get(user=current_user)
-    
+
+    # Check if the user to unfollow is in the current user's following list
     if user_to_unfollow not in current_user_profile.following.all():
+        # If not, return a 400 error response
         return Response({"error": "You are not following this user"}, status=status.HTTP_400_BAD_REQUEST)
-    
+
+    # Remove the user to unfollow from the current user's following list
     current_user_profile.following.remove(user_to_unfollow)
+    # Save the user profile
     current_user_profile.save()
-    
+
+    # Return a success response
     return Response({"success": "User unfollowed successfully"}, status=status.HTTP_200_OK)
 
 
