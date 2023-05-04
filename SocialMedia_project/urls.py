e5a318b3f931d3b00086305ce1cd4a675b825a64
OLUBUNMI OLUWATOBI JAMES <82912148+BrodaOJ56@users.noreply.github.com>
2023-05-04 21:02:52 +0100

    drf-spectacular url  path

--- SocialMedia_project/urls.py
+++ SocialMedia_project/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ReachOut2Me.urls')),
-    path('api/dj-rest-auth/', include('dj_rest_auth.urls'))
+    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
