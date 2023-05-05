88bc3e6d33ab173d8a0202ca24445e25df95fdba
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-05 18:35:20 +0100

    set the accout url for the reverse action from the registration url

--- SocialMedia_project/urls.py
+++ SocialMedia_project/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ReachOut2Me.urls')),
     path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('accounts/', include('allauth.urls')),
+    # path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
     # path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
