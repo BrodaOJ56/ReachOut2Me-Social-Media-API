772c5ca5deaef7f9ed7c5390a8a4c052e6c957d8
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-08 12:09:02 +0100

    test get all posts

--- ReachOut2Me/tests/test_posts.py
+++ ReachOut2Me/tests/test_posts.py
@@ -3,7 +3,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
 from django.contrib.auth.models import User
-from ..models import UserProfile, Post
+from ..models import Post
 from rest_framework.authtoken.models import Token
 
 
@@ -15,12 +15,11 @@ class PostTestCase(TestCase):
         """Define the test client and other test variables."""
         self.user = User.objects.create_user(
             username="testuser",
-            password="testpassword",
+            password="testpasswordForUs",
             first_name="testfirstname",
             last_name="testlastname",
             email="test@email.com",
         )
-        UserProfile.objects.create(user=self.user)
         self.token = Token.objects.create(user=self.user)
         Post.objects.create(author=self.user, content="test content")
         self.client = APIClient()
@@ -32,32 +31,32 @@ class PostTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
-    def test_create_post(self):
-        """Test the api can create a post."""
-        response = self.client.post(self.url, data={"content": "test content"})
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data["content"], "test content")
-        self.assertEqual(response.data["author"], self.user.username)
-
-    def test_get_a_post_by_id(self):
-        """Test the api can get a post by id."""
-        url = reverse("post_detail", kwargs={"pk": 1})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["content"], "test content")
-        self.assertEqual(response.data["author"], self.user.username)
-
-    def test_delete_a_post_by_id(self):
-        """Test the api can delete a post by id."""
-        url = reverse("post_detail", kwargs={"pk": 1})
-        response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Post.objects.count(), 0)
-
-    def test_update_a_post_by_id(self):
-        """Test the api can update a post by id"""
-        url = reverse("post_detail", kwargs={"pk": 1})
-        response = self.client.put(url, data={"content": "test content updated"})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["content"], "test content updated")
-        self.assertEqual(response.data["author"], self.user.username)
+#     def test_create_post(self):
+#         """Test the api can create a post."""
+#         response = self.client.post(self.url, data={"content": "test content"})
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#         self.assertEqual(response.data["content"], "test content")
+#         self.assertEqual(response.data["author"], self.user.username)
+#
+#     def test_get_a_post_by_id(self):
+#         """Test the api can get a post by id."""
+#         url = reverse("post_detail", kwargs={"pk": 1})
+#         response = self.client.get(url)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#         self.assertEqual(response.data["content"], "test content")
+#         self.assertEqual(response.data["author"], self.user.username)
+#
+#     def test_delete_a_post_by_id(self):
+#         """Test the api can delete a post by id."""
+#         url = reverse("post_detail", kwargs={"pk": 1})
+#         response = self.client.delete(url)
+#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+#         self.assertEqual(Post.objects.count(), 0)
+#
+#     def test_update_a_post_by_id(self):
+#         """Test the api can update a post by id"""
+#         url = reverse("post_detail", kwargs={"pk": 1})
+#         response = self.client.put(url, data={"content": "test content updated"})
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#         self.assertEqual(response.data["content"], "test content updated")
+#         self.assertEqual(response.data["author"], self.user.username)
